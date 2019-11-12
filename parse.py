44ca3f228bf502dd4f529cecb6870775769e279a
Onur Çopur <onurcopur12@gmail.com>
2019-11-12 20:57:46 +0100

    Add files via upload

--- parse.py
+++ parse.py
@@ -2,15 +2,16 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
 
-columns = ["title", "intro", "plot", "film_name", "director", "producer", "writer", "starring", "music", "release_date", "runtime", "country", "language", "budget"]
+columns = ["title", "intro", "plot", "film_name", "director", "producer", "writer", "starring", "music", "release_date","running time", "country", "language", "budget"]
 a = np.empty((10000, 14,))
 a[:] = np.nan
 df = pd.DataFrame(data=a, columns=columns)
 
-for i in range(9997):
-    soup = BeautifulSoup(open("movies\\article_"+str(i)+".html", encoding="utf8"), "html.parser")
+for film in range(10): # the index should be the number of the total files here 10 is just for quick trial :
+    soup = BeautifulSoup(open("movies\\article_"+str(film)+".html", encoding="utf8"), "html.parser")
     #exctract the title
-    df.iloc[i, 0] = soup.find("h1").text
+    df.iloc[film, 0] = soup.find("h1").text
+    df.iloc[film, 3] = soup.find("h1").text
     #extract the Intro
     Intro = ""
     par = soup.p # first paragraph of the html page
@@ -18,7 +19,7 @@ for i in range(9997):
         if par.name == "p":
             Intro += par.get_text()
         par = par.next_element
-    df.iloc[i, 1]=Intro
+    df.iloc[film, 1] = Intro
 
     # Parse the Plot
     for heading in soup.find_all(["h2", "h3"]):
@@ -35,7 +36,38 @@ for i in range(9997):
             if heading.name == "p":
                 Plot += heading.get_text()
             heading = heading.next_element
-        df.iloc[i, 2] = Plot
+        df.iloc[film, 2] = Plot
     except AttributeError:
-        continue
+        pass
 
+    # Info box Parsing
+
+    info_box = soup.find("table", {"class": "infobox vevent"})
+    if info_box is None:
+        continue
+    tags = info_box.contents[0].contents # find tags in infobox
+    for tr in tags:
+        if len(tr.contents) == 2:
+            flag = True
+            s = tr.contents[0].get_text() # get the headings in infobox
+            for i in range(len(columns)):
+                sub_Str = s[:4].lower()
+                target_str = columns[i]
+                if sub_Str in target_str: # find correct column to write the relevent information
+                    flag = False
+                    break
+            if flag:
+                continue
+            l = []
+            if len(tr.contents[1].contents) > 1:
+                for j in tr.contents[1].contents:
+                    if j.string is not None:
+                        l.append(j.string)
+                df.iloc[film, i] = " ".join(l)
+            else:
+                df.iloc[film, i] = [tr.contents[1].get_text()]
+
+    df.iloc[film, :].to_csv("parsed\\"+str(film)+".tsv", sep='\t', encoding='utf-8') # create a different folder called parsed and save the tsv files in it
+
+
+#df.iloc[0,:].to_excel("parsed\\parsing.xlsx")
