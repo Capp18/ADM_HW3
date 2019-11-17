6a44baf1e9214f96c1f9f273755866987aa9d8fc
Dario Cappelli <55833431+Capp18@users.noreply.github.com>
2019-11-17 22:40:06 +0100

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,13 @@
+import time
+import urllib.request
+from bs4 import BeautifulSoup
+from random import randint
+import pandas as pd
+import csv
+from sklearn.feature_extraction.text import TfidfVectorizer
+from bs4 import BeautifulSoup
+import numpy as np
+from nltk.tokenize import word_tokenize, RegexpTokenizer
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import string
